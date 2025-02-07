aa79e06caa10555814629c53a5349a88017b1e8f
Thomas Brickhouse <thomas@halebob.net>
2025-02-07 11:30:00 -0500

    import working dir

---
+++ thought_swap/thought_swap_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ThoughtSwapAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "thought_swap.thought_swap_app"
---
+++ thought_swap/users/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from thought_swap.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer[User]):
+    class Meta:
+        model = User
+        fields = ["name", "url"]
+
+        extra_kwargs = {
+            "url": {"view_name": "api:user-detail", "lookup_field": "pk"},
+        }
