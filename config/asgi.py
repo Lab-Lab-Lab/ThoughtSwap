3908ba7bc7f7825736fcdd15c98b92151982bc62
Michael Stewart <stewarmc@jmu.edu>
2025-04-07 22:32:05 -0400

    move chat

--- config/asgi.py
+++ config/asgi.py
@@ -4,6 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from thoughtswap.thoughtswap.routing import websocket_urlpatterns
+from thoughtswap.chat.routing import websocket_urlpatterns as thoughtswap_chat_routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thoughtswap.settings")
 
@@ -14,7 +15,7 @@ application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns+thoughtswap_chat_routing))
         ),
     }
 )
