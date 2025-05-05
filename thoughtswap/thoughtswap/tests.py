5eea64abc9f24658d63d2e8cb411606f956f20b0
Michael Stewart <stewarmc@jmu.edu>
2025-05-05 09:16:12 -0400

    adding tests

--- thoughtswap/thoughtswap/tests.py
+++ thoughtswap/thoughtswap/tests.py
@@ -1,3 +1,157 @@
-from django.test import TestCase
+# chat/tests.py
+from channels.testing import ChannelsLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 
-# Create your tests here.
+
+class ChatTests(ChannelsLiveServerTestCase):
+    serve_static = True  # emulate StaticLiveServerTestCase
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        try:
+            # NOTE: Requires "chromedriver" binary to be installed in $PATH
+            cls.driver = webdriver.Chrome()
+            cls.other_driver = webdriver.Firefox()
+        except Exception as e:
+            super().tearDownClass()
+            raise e
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def test_when_chat_message_posted_then_seen_by_everyone_in_same_room_two_browsers(
+        self,
+    ):
+        try:
+            self._open_teacher_and_student_dashboards()
+            # self._enter_chat_room("room_1")
+
+            # self._open_new_window()
+            # self._enter_chat_room("room_1")
+
+            self._switch_to_window(0)
+            self._post_message("hello")
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self._chat_log_value,
+                "Message was not received by window 1 from window 1",
+            )
+            self._switch_to_window(1)
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self._chat_log_value,
+                "Message was not received by window 2 from window 1",
+            )
+        finally:
+            self._close_all_new_windows()
+
+    def test_when_chat_message_posted_then_seen_by_everyone_in_same_room(self):
+        try:
+            self._enter_chat_room("room_1")
+
+            self._open_new_window()
+            self._enter_chat_room("room_1")
+
+            self._switch_to_window(0)
+            self._post_message("hello")
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self._chat_log_value,
+                "Message was not received by window 1 from window 1",
+            )
+            self._switch_to_window(1)
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self._chat_log_value,
+                "Message was not received by window 2 from window 1",
+            )
+        finally:
+            self._close_all_new_windows()
+
+    def test_when_chat_message_posted_then_not_seen_by_anyone_in_different_room(self):
+        try:
+            self._enter_chat_room("room_1")
+
+            self._open_new_window()
+            self._enter_chat_room("room_2")
+
+            self._switch_to_window(0)
+            self._post_message("hello")
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self._chat_log_value,
+                "Message was not received by window 1 from window 1",
+            )
+
+            self._switch_to_window(1)
+            self._post_message("world")
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "world" in self._chat_log_value,
+                "Message was not received by window 2 from window 2",
+            )
+            self.assertTrue(
+                "hello" not in self._chat_log_value,
+                "Message was improperly received by window 2 from window 1",
+            )
+        finally:
+            self._close_all_new_windows()
+
+    # === Utility ===
+
+    def _open_teacher_and_student_dashboards(self):
+        self.driver.get(self.live_server_url + "/thoughtswap/teacher_dashboard/")
+        elem = self.driver.find_element(By.ID, "id_login")
+        ActionChains(self.driver).send_keys("teacher", Keys.ENTER).perform()
+        self.other_driver.get(self.live_server_url + "/thoughtswap/dashboard/")
+        WebDriverWait(self.driver, 2).until(
+            lambda _: "teacher" in self.driver.current_url
+        )
+        self.other_driver.get(self.live_server_url + "/student/")
+        WebDriverWait(self.other_driver, 2).until(
+            lambda _: "student" in self.other_driver.current_url
+        )
+
+    def _enter_chat_room(self, room_name):
+        self.driver.get(self.live_server_url + "/chat/")
+        ActionChains(self.driver).send_keys(room_name, Keys.ENTER).perform()
+        WebDriverWait(self.driver, 2).until(
+            lambda _: room_name in self.driver.current_url
+        )
+
+    # def _enter_chat_room(self, room_name):
+    #     self.driver.get(self.live_server_url + "/chat/")
+    #     ActionChains(self.driver).send_keys(room_name, Keys.ENTER).perform()
+    #     WebDriverWait(self.driver, 2).until(
+    #         lambda _: room_name in self.driver.current_url
+    #     )
+
+    def _open_new_window(self):
+        self.driver.execute_script('window.open("about:blank", "_blank");')
+        self._switch_to_window(-1)
+
+    def _open_new_window_other_client(self):
+        self.other_driver.execute_script('window.open("about:blank", "_blank");')
+        self._switch_to_window(-1)
+
+    def _close_all_new_windows(self):
+        while len(self.driver.window_handles) > 1:
+            self._switch_to_window(-1)
+            self.driver.execute_script("window.close();")
+        if len(self.driver.window_handles) == 1:
+            self._switch_to_window(0)
+
+    def _switch_to_window(self, window_index, driver=None):
+        if driver is None:
+            driver = self.driver
+        driver.switch_to.window(driver.window_handles[window_index])
+
+    def _post_message(self, message):
+        ActionChains(self.driver).send_keys(message, Keys.ENTER).perform()
+
+    @property
+    def _chat_log_value(self):
+        return self.driver.find_element(
+            by=By.CSS_SELECTOR, value="#chat-log"
+        ).get_property("value")
